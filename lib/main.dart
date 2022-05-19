80489f1f0f02a83b0c8c30f0bb03c2053cf3d132
nikhiljose47 <nikhiljose47@gmail.com>
2022-05-19 15:25:29 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  static const String _title = 'My list';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      home: Scaffold(
+        appBar: AppBar(title: const Text(_title)),
+        body: const MyStatefulWidget(),
+      ),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  List<String> items = ['5 new knowledge index', 'life up', 'empire new baby block' ];
+
+  @override
+  Widget build(BuildContext context) {
+Widget listView = ListView.builder(
+      itemCount: items.length,
+      padding: const EdgeInsets.symmetric(vertical: 16),
+      itemBuilder: (BuildContext context, int index) {
+        return Dismissible(
+          background: Container(
+            color: Colors.green,
+          ),
+          key: ValueKey<String>(items[index]),
+          onDismissed: (DismissDirection direction) {
+            setState(() {
+              items.removeAt(index);
+            });
+          },
+          child: ListTile(
+            title: Text(
+              'Item ${items[index]}',
+            ),
+          ),
+        );
+      },
+    );
+  
+    return Column(
+      children: [
+        Expanded(flex:3,child: listView),
+        const Expanded(child: Text("Upcoming - day auto refresh,card add, chart, canvas model cards, pipelines, self customizable all, "))
+
+      ],
+    );
+ 
+  }
+}
